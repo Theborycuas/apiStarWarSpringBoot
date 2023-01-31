ddf0c745225c6954a331cde5726e78e76353811a
Borys <borys_jair@hotmail.com>
2023-01-31 12:51:43 -0500

    Ready Delete

--- src/main/java/com/example/pruebaunity/controllers/CharacterController.java
+++ src/main/java/com/example/pruebaunity/controllers/CharacterController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.pruebaunity.services.CharacterService;
@@ -39,12 +40,24 @@ public class CharacterController {
 		}
 
 	}
-	@DeleteMapping("/delete")
-	public ResponseEntity<Void> eliminar(@RequestBody Personaje characterRequest){
-		characterService.delete(characterRequest);
+	
+	@PostMapping("/update")
+	public ResponseEntity<Personaje> update(@RequestBody Personaje characterRequest){
+		Personaje newCharacter = characterService.create(characterRequest);
+		try {
+			return ResponseEntity.created(new URI("/character"+newCharacter.getId())).body(newCharacter);
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+
+	}
+	
+	@GetMapping("/delete/{id}")
+	public ResponseEntity<Void> eliminarId(@PathVariable Long id){
+		characterService.deleteId(id);
 		return ResponseEntity.ok().build();
 	}
-
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Optional<Personaje>> listarById(@PathVariable("id") Long id){
 		return ResponseEntity.ok(characterService.findById(id));
